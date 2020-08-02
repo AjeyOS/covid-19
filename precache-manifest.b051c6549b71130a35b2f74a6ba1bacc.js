2fbfb80c53a514b99ff4eaf967654c344db49fd6
ajay2323 <ajay.gh2323@gmail.com>
2020-08-02 17:13:14 +0530

    Updates

---
+++ precache-manifest.b051c6549b71130a35b2f74a6ba1bacc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cedb933fc24c08a4453d72220d79c29",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "d0f99cc47105e6e39c54",
+    "url": "/covid-19/static/css/main.0577db47.chunk.css"
+  },
+  {
+    "revision": "34d6b8ff0924a5fb81a3",
+    "url": "/covid-19/static/js/2.7ce67059.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.7ce67059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0f99cc47105e6e39c54",
+    "url": "/covid-19/static/js/main.86f0a46a.chunk.js"
+  },
+  {
+    "revision": "3163881592563347c15b",
+    "url": "/covid-19/static/js/runtime-main.ab45cd9d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
